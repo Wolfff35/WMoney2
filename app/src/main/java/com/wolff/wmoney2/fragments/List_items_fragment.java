a87f5d95a2671946ec06a83588235354ca82dbc6
wolfff <wolfff35@gmail.com>
2018-02-01 14:09:51 +0200

    01/02/2018 from office 2

--- app/src/main/java/com/wolff/wmoney2/fragments/List_items_fragment.java
+++ app/src/main/java/com/wolff/wmoney2/fragments/List_items_fragment.java
@@ -1,7 +1,14 @@
 package com.wolff.wmoney2.fragments;
 
+import android.app.ListFragment;
 import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
 
+import com.wolff.wmoney2.localdb.DbGetData;
 import com.wolff.wmoney2.model.WBase;
 
 import java.util.ArrayList;
@@ -17,11 +24,38 @@ public class List_items_fragment<T extends WBase> extends AList_fragment {
     public interface List_items_fragment_listener{
         void onItemClick();
     }
- //   public static List_items_fragment newInstance(){
- //       List_items_fragment<> fragment = new List_items_fragment();
- //       return
- //   }
- @Override
+    public static<T extends WBase> List_items_fragment newInstance(){
+        List_items_fragment<T> fragment = new List_items_fragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //mList = DbGetData.get(getContext()).getList(T.);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //mAccountList = DataLab.get(getContext()).getWAccountList(getContext());
+        onActivityCreated(null);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //ListAdapter adapter = new Adapter_AccountList(getContext(),mAccountList);
+        //setListAdapter(adapter);
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            //    listener.onAccountItemClick(mAccountList.get(position));
+            }
+        });
+    }
+
+    @Override
  public void onAttach(Context context) {
      super.onAttach(context);
      listener = (List_items_fragment_listener) context;
